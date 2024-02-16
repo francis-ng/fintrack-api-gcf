93b10e6f9c4c9845b70c6cd31830ae235ed5ccc3
Francis Ng <francis-ng@outlook.com>
2024-02-16 15:01:06 +0900

    WIP

--- UserLogin/Function.cs
+++ UserLogin/Function.cs
@@ -1,9 +1,15 @@
 using Google.Cloud.Functions.Framework;
 using Microsoft.AspNetCore.Http;
+using UserLogin.Models;
+using MongoDB.Driver;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.IO;
+using System.Text.Json;
 
-namespace User
+namespace UserLogin
 {
     public class Function : IHttpFunction
     {
@@ -16,12 +22,23 @@ namespace User
         {
             if (context.Request.Method == "POST")
             {
+                var client = new MongoClient("connectionString");
+                var users = client.GetDatabase("databaseName").GetCollection<User>("userCollectionName");
 
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    await context.Request.Body.CopyToAsync(ms);
+                    ms.Seek(0, SeekOrigin.Begin);
+                    var content = await JsonSerializer.DeserializeAsync(ms);
+                }
+
+                users.Find(user => user.UserName == name).FirstOrDefault();
             }
             else
             {
                 context.Response.StatusCode = (int)HttpStatusCode.MethodNotAllowed;
                 await context.Response.CompleteAsync();
+                return;
             }
         }
     }
---
+++ UserLogin/User.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace UserLogin.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public byte[] Salt { get; set; }
+
+        public string RefreshToken { get; set; }
+
+        public string AccessToken { get; set; }
+
+        public DateTime AccessExpiry { get; set; }
+    }
+}
